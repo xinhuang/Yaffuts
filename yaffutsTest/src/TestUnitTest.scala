8f8fe3cfdcd70925916df654caf8d5bfb440f7f0
Patz Huang <walkaniceday@gmail.com>
2012-12-17 22:39:10 +0800

    TestUnitTest use new test definition method.

--- yaffutsTest/src/TestUnitTest.scala
+++ yaffutsTest/src/TestUnitTest.scala
@@ -1,14 +1,9 @@
 import yaffuts.{TestMethodCollection, Test}
 
 class TestUnitTest extends Test {
+  override val testMethods: TestMethodCollection = new TestMethodCollection()
 
-  override val testMethods: TestMethodCollection = new TestMethodCollection(
-    ("given_assert_fail_execute_should_fail_total_increase", given_assert_fail_execute_should_fail_total_increase _),
-    ("given_throw_null_exception_should_fail_total_increase", given_throw_null_exception_should_fail_total_increase _)
-  )
-
-
-  def given_assert_fail_execute_should_fail_total_increase() {
+  test ("given assert fail execute should fail total increase") {
     val sut = new AssertFailTestClass
 
     sut.run()
@@ -16,7 +11,7 @@ class TestUnitTest extends Test {
     Assert.areEqual(sut.expectFailure, sut.failTotal)
   }
 
-  def given_throw_null_exception_should_fail_total_increase() {
+  test ("given throw null exception should fail total increase") {
     val sut = new ThrowNullExceptionTestClass
 
     sut.run()
