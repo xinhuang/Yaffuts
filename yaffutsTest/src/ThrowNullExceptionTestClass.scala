7f219689c62db6be7eb705312348d8cf026a0f39
Patz Huang <walkaniceday@gmail.com>
2012-12-17 22:38:10 +0800

    ThrowNullExceptionTestClass use new test definition method.

--- yaffutsTest/src/ThrowNullExceptionTestClass.scala
+++ yaffutsTest/src/ThrowNullExceptionTestClass.scala
@@ -1,13 +1,11 @@
 import yaffuts.{TestMethodCollection, Test}
 
 class ThrowNullExceptionTestClass extends Test {
-  override val testMethods: TestMethodCollection = new TestMethodCollection(
-    ("given_access_a_null_reference_should_throw_exception", given_access_a_null_reference_should_throw_exception _)
-  )
+  override val testMethods: TestMethodCollection = new TestMethodCollection
 
   val expectFailure = 1
 
-  def given_access_a_null_reference_should_throw_exception() {
+  test("given access a null reference should throw exception") {
     val someVar: String = null
 
     someVar.contains("don't care")
